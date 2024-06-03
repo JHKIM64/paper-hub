49efae03ecb35539647a155357fff8bda24cfc11
High-Summer <yahjune@gmail.com>
2024-06-03 21:54:40 +0900

    0602

--- backend/vector_search/embedding.py
+++ backend/vector_search/embedding.py
@@ -1,6 +1,14 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 
+# openai 사용시
+    # openai.api_key = '~~~  sectret key ~~~'
+    # def get_embedding_openai(text):
+    #     response = openai.Embedding.create(
+    #         input=text,
+    #         model="text-embedding-ada-002"
+    #     )
+    #     return response['data'][0]['embedding']
 def get_embedding(text, model, tokenizer):
     inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True, max_length=512)
     with torch.no_grad():
@@ -10,6 +18,10 @@ def get_embedding(text, model, tokenizer):
 
 def model_select(model_name="sentence-transformers/all-MiniLM-L6-v2"):
     global model, tokenizer
+    # mteb sota
+    # https://huggingface.co/nvidia/NV-Embed-v1
+    # model_name = "nvidia/NV-Embed-v1"
+
     if model_name == "sentence-transformers/all-MiniLM-L6-v2":
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModel.from_pretrained(model_name)
